678641f4933a0c277e4ad2dcd340c793958b62f8
vaniekwan <stevaniekwan@hotmail.com>
2024-12-04 11:35:24 -0500

    test

---
+++ landingPage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Outfit&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+        <link href="css/style_land.css" rel="stylesheet" />
+        <title>Document</title>
+    <title>Document</title>
+</head>
+<body>
+            <div class="div_box">
+                <span class="titre">Welcome to LMMed, Where...</span>
+                <span class="sous_titre">description de l’application: text</span>
+                <div class="bouton_login_pos">
+                    <div class="div_login"></div>
+                </div>
+                <a class="login_text" href=/footer.php>Login</a>
+                <div class="bouton_create_pos">
+                    <div class="div_create"></div>
+                </div>
+                <a class="create_text" href=/footer.php>Create</a>
+                <div class="logo-container">
+                    <img src="/images/Logo.png" alt="Company Logo">
+                </div>
+                <div class="nav_bar">
+                    <a class="nav_text about_pos" href=/footer.php>About</a>
+                    <a class="nav_text contact_pos"href=/footer.php>Contact</a>
+                    <a class="nav_text nav_login_pos" href=/footer.php>Login</a>
+                </div>
+                <div class="image_background"></div>
+            </div>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>DocuPet</title>
+</head>
+<body>
+            <div class="div_box">
+                <span class="titre">Tell Us About your dog</span>
+                <div class="bouton_login_pos">
+                    <div class="div_login"></div>
+                </div>
+                <a class="login_text" href=/footer.php>Login</a>
+                <div class="bouton_create_pos">
+                    <div class="div_create"></div>
+                </div>
+                <a class="create_text" href=/footer.php>Create</a>
+                <div class="logo-container">
+                    <img src="/images/Logo.png" alt="Company Logo">
+                </div>
+                <div class="nav_bar">
+                    <a class="nav_text about_pos" href=/footer.php>Help</a>
+                    <a class="nav_text contact_pos"href=/footer.php>Save and Finish Later</a>
+                </div>
+            </div>
+</body>
+</html>
